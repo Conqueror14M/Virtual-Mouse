7a48194e984227bd77097efc7b0dc6a757c4db97
Conqueror14M <dy6579@srmist.edu.in>
2021-09-03 23:38:32 +0530

    fixed tipId error

--- HandTrackerModule.py
+++ HandTrackerModule.py
@@ -14,6 +14,7 @@ class handDetector():
 		self.mpHands = mp.solutions.hands
 		self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
 		self.mpDraw = mp.solutions.drawing_utils
+		self.tipIds = [4, 8, 12, 16, 20]
 
 	def findHands(self, img, draw = True):
 		imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -26,71 +27,70 @@ class handDetector():
 		return img
 
 
-    def findPosi(self, img, handNo=0, draw=True):
+	def findPosi(self, img, handNo=0, draw=True):
+		xList = []
+		yList = []
+		bbox = []
+		bboxInfo =[]
+		self.lmList = []
+		if self.results.multi_hand_landmarks:
+			myHand = self.results.multi_hand_landmarks[handNo]
+			for id, lm in enumerate(myHand.landmark):
+				h, w, c = img.shape
+				px, py = int(lm.x * w), int(lm.y * h)
+				xList.append(px)
+				yList.append(py)
+				self.lmList.append([px, py])
+				if draw:
+					cv2.circle(img, (px, py), 5, (255, 0, 255), cv2.FILLED)
+			xmin, xmax = min(xList), max(xList)
+			ymin, ymax = min(yList), max(yList)
+			boxW, boxH = xmax - xmin, ymax - ymin
+			bbox = xmin, ymin, boxW, boxH
+			cx, cy = bbox[0] + (bbox[2] // 2), \
+					 bbox[1] + (bbox[3] // 2)
+			bboxInfo = {"id": id, "bbox": bbox,"center": (cx, cy)}
 
-        xList = []
-        yList = []
-        bbox = []
-        bboxInfo =[]
-        self.lmList = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                h, w, c = img.shape
-                px, py = int(lm.x * w), int(lm.y * h)
-                xList.append(px)
-                yList.append(py)
-                self.lmList.append([px, py])
-                if draw:
-                    cv2.circle(img, (px, py), 5, (255, 0, 255), cv2.FILLED)
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
-            boxW, boxH = xmax - xmin, ymax - ymin
-            bbox = xmin, ymin, boxW, boxH
-            cx, cy = bbox[0] + (bbox[2] // 2), \
-                     bbox[1] + (bbox[3] // 2)
-            bboxInfo = {"id": id, "bbox": bbox,"center": (cx, cy)}
+			if draw:
+				cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+							  (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+							  (0, 255, 0), 2)
 
-            if draw:
-                cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
-                              (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
-                              (0, 255, 0), 2)
-
-        return self.lmList, bboxInfo
-
+		return self.lmList, bboxInfo
 
 
-    def fingersUp(self):
 
-        if self.results.multi_hand_landmarks:
-            myHandType = self.handType()
-            fingers = []
-            # Thumb
-            if myHandType == "Right":
-                if self.lmList[self.tipIds[0]][0] > self.lmList[self.tipIds[0] - 1][0]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
-            else:
-                if self.lmList[self.tipIds[0]][0] < self.lmList[self.tipIds[0] - 1][0]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
+	def fingersUp(self):
 
-            # 4 Fingers
-            for id in range(1, 5):
-                if self.lmList[self.tipIds[id]][1] < self.lmList[self.tipIds[id] - 2][1]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
-        return fingers
+		if self.results.multi_hand_landmarks:
+			myHandType = self.handType()
+			fingers = []
+			# Thumb
+			if myHandType == "Right":
+				if self.lmList[self.tipIds[0]][0] > self.lmList[self.tipIds[0] - 1][0]:
+					fingers.append(1)
+				else:
+					fingers.append(0)
+			else:
+				if self.lmList[self.tipIds[0]][0] < self.lmList[self.tipIds[0] - 1][0]:
+					fingers.append(1)
+				else:
+					fingers.append(0)
 
-    def handType(self):
-        if self.results.multi_hand_landmarks:
-            if self.lmList[17][0] < self.lmList[5][0]:
-                return "Right"
-            else:
-                return "Left"
+			# 4 Fingers
+			for id in range(1, 5):
+				if self.lmList[self.tipIds[id]][1] < self.lmList[self.tipIds[id] - 2][1]:
+					fingers.append(1)
+				else:
+					fingers.append(0)
+		return fingers
+
+	def handType(self):
+		if self.results.multi_hand_landmarks:
+			if self.lmList[17][0] < self.lmList[5][0]:
+				return "Right"
+			else:
+				return "Left"
 
 
 def main():
