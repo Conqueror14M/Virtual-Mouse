b37c7fe2e99d8732ba98b822952459eb490b295d
Conqueror14M <dy6579@srmist.edu.in>
2021-09-05 22:08:40 +0530

    added Distance method

--- HandTrackerModule.py
+++ HandTrackerModule.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
-
+import math
 
 
 class handDetector():
@@ -84,6 +84,21 @@ class handDetector():
 				else:
 					fingers.append(0)
 		return fingers
+
+	def findDistance(self, p1, p2, img, draw=True):
+		if self.results.multi_hand_landmarks:
+			x1, y1 = self.lmList[p1][0], self.lmList[p1][1]
+			x2, y2 = self.lmList[p2][0], self.lmList[p2][1]
+			cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+			if draw:
+				cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+				cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+				cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+				cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+			length = math.hypot(x2 - x1, y2 - y1)
+			return length, img, [x1, y1, x2, y2, cx, cy]
 
 	def handType(self):
 		if self.results.multi_hand_landmarks:
